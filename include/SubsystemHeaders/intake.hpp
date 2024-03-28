93de9a877a7e2a43be90cde8d3cb8d89548341e0
Dilpreet <67494637+Dilpreet05@users.noreply.github.com>
2024-03-27 20:44:41 -0400

    top motor intake

--- include/SubsystemHeaders/intake.hpp
+++ include/SubsystemHeaders/intake.hpp
@@ -1,6 +1,8 @@
 #include "main.h"
 
 extern pros::Motor intakeMotor;
+extern pros::Motor intakeMotorTop;
+
 
 void setIntakeBrakes();
 void spinIntake();
--- src/SubsystemFiles/intake.cpp
+++ src/SubsystemFiles/intake.cpp
@@ -1,6 +1,7 @@
 #include "main.h"
 
 pros::Motor intakeMotor(19,MOTOR_GEARSET_06,false);
+pros::Motor intakeMotorTop(16,MOTOR_GEARSET_6,false);
 
 void setIntakeBrakes(){
         intakeMotor.set_brake_mode(MOTOR_BRAKE_COAST);
@@ -10,10 +11,13 @@ void spinIntake(){
 
         if(master.get_digital(pros::E_CONTROLLER_DIGITAL_L1) && !(master.get_digital(pros::E_CONTROLLER_DIGITAL_L2))){
                 intakeMotor = 127;
+                intakeMotorTop = 127;
         }else if(master.get_digital(pros::E_CONTROLLER_DIGITAL_L2) && !(master.get_digital(pros::E_CONTROLLER_DIGITAL_L1))){
                 intakeMotor = -127;
+                intakeMotorTop = -127;
         }else{
                 intakeMotor.brake();
+                intakeMotorTop.brake();
         }
         
         pros::delay(20);
